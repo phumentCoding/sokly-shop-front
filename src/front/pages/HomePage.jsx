d49299f43e8f24d321c9e8394ea06e9454ddd18e
phumentCoding <phumentgaming@gmail.com>
2025-07-03 22:46:07 +0700

    apply slide and brand components

---
+++ src/front/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeSlide from '../components/HomeSlide'
+
+const HomePage = () => {
+  return (
+    <div>
+        <HomeSlide/>
+    </div>
+  )
+}
+
+export default HomePage
--- src/front/router/frontRoute.jsx
+++ src/front/router/frontRoute.jsx
@@ -1,18 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Master from "../layout/Master";
+import HomePage from "../pages/HomePage";
 
 const FrontRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Master/>}>
-               <Route
-                    index
-                    element={
-                        <div className="p-8">
-                        <h1 className="text-2xl font-bold">Welcome to Sokly Phone Shop</h1>
-                        </div>
-                    }
-                />
+               <Route index element={<HomePage/>}/>
             </Route>
         </Routes>
     )
