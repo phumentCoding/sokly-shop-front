20914c4d5f9657c52ee3e1d31e7f230edb78edad
Yangchanthorony <tonyja2460@gmail.com>
2025-07-14 21:54:41 +0700

    ddd

--- src/front/pages/produtsPage/components/Nabar.jsx
+++ src/front/pages/produtsPage/components/Nabar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+"use client"
 
-const Nabar = ({items}) => {
+const Nabar = ({ items }) => {
   return (
-    <div>Nabar</div>
+    <div className="w-full sm:max-w-7xl mx-auto px-4 sm:px-6 py-4">
+      <nav className="flex flex-wrap items-center gap-2 text-xs sm:text-sm text-gray-600">
+        <span className="hover:text-blue-600 cursor-pointer truncate">Home</span>
+        <span className="mx-1 sm:mx-2">.</span>
+        <span className="hover:text-blue-600 cursor-pointer truncate">Prodeucts</span>
+        <span className="mx-1 sm:mx-2">.</span>
+        <span className="hover:text-blue-600 cursor-pointer truncate">Apple</span>
+        <span className="mx-1 sm:mx-2">.</span>
+        <span className="text-gray-900 font-medium truncate">iphone</span>
+      </nav>
+    </div>
   )
 }
 
--- src/front/pages/produtsPage/view/Product.jsx
+++ src/front/pages/produtsPage/view/Product.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import Navbar from '../../productDetail/components/Navbar'
+
+import Nabar from '../components/Nabar'
+import FilterProduct from '../components/FilterProduct'
 
 const Product = () => {
   return (
     <div>
-        <Navbar items={[]}/>
-        <h1 className='max-w-7xl mx-auto px-6 '>Mobile Phone</h1>
+      
+       <Nabar items={[]}/>
+       {/* <FilterProduct/> */}
+        <FilterProduct/>
         
 
         {/* Reusable component call here */}
