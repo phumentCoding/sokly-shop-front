cd774fdf06fdf4586b81dc4125d0b6c04f29a50b
Phal Raksa <raksaphal14@gmail.com>
2025-07-23 21:36:55 +0700

    account page  adding

---
+++ src/front/pages/account/view/AccountManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Favorite from '../components/Favorite'
+
+const AccountManagement = () => {
+  return (
+    <div>
+      <Favorite/>
+    </div>
+  )
+}
+
+export default AccountManagement
